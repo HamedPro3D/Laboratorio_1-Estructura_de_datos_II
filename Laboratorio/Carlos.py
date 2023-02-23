ec0dfda4de4c7bc4bf232fe2fb6f3a87d9655f4b
HamedPro3D <110119103+HamedPro3D@users.noreply.github.com>
2023-02-23 14:34:41 -0500

    codigo 1.1

--- Laboratorio/Carlos.py
+++ Laboratorio/Carlos.py
@@ -23,6 +23,9 @@ fuente = pygame.font.SysFont('Arial', 15)
 # Definimos el tamaño visual que tendra el nodo     
 NODO_TAMAÑO = 15     
 
+#Creacion minijuego
+class MiniJuego:
+    ...
 # Creamos la clase nodo
 class Nodo:     
     def __init__(self, valor, x, y):     
@@ -32,27 +35,27 @@ class Nodo:
         self.x = x     
         self.y = y     
 
-# Creamos la clase del arbol (en este caso binario) para agregarle los datos
-class ArbolBinario:     
+# Creamos la clase del arbol (en este caso binario) para agregarle los 
+class ArbolBinario1:     
     def __init__(self):     
         self.padre = None     
 
         # Funcion que añade los valores a los nodos y creacion del arbol
     def Añadirnodo(self, valor):     
         if self.padre is None:     
-            self.padre = Nodo(valor, ANCHO//2, 50)     
+            self.padre = Nodo(valor, ANCHO/3.5, 50)     
         else:     
-            self.AgregarNodo(valor, self.padre, ANCHO//2, 60, 1)     
-    def AgregarNodo(self, valor, nodo, x, y, level):     
+            self.AgregarNodo(valor, self.padre, ANCHO/3.5, 100, 1)     
+    def AgregarNodo(self, valor, nodo, x, y, nivel):     
         if nodo.izquierda is None:     
-            nodo.izquierda = Nodo(valor, x-2**(6-level), y+NODO_TAMAÑO*2)     
+            nodo.izquierda = Nodo(valor, x-4*(6-nivel), y+NODO_TAMAÑO*2)     
         elif nodo.derecha is None:     
-            nodo.derecha = Nodo(valor, x+2**(6-level), y+NODO_TAMAÑO*2)     
+            nodo.derecha = Nodo(valor, x+4*(6-nivel), y+NODO_TAMAÑO*2)     
         else:     
             if valor < nodo.valor:     
-                self.AgregarNodo(valor, nodo.izquierda, x-2**(6-level), y+NODO_TAMAÑO*3, level+1)     
+                self.AgregarNodo(valor, nodo.izquierda, x-2**(6-nivel), y+NODO_TAMAÑO*4, nivel+1)     
             else:     
-                self.AgregarNodo(valor, nodo.derecha, x+2**(6-level), y+NODO_TAMAÑO*3, level+1)     
+                self.AgregarNodo(valor, nodo.derecha, x+2**(6-nivel), y+NODO_TAMAÑO*4, nivel+1)     
     def draw(self, pantalla, fuente):     
         self.DibujarNodo(pantalla, fuente, self.padre)     
     def DibujarNodo(self, pantalla, fuente, nodo):     
@@ -68,8 +71,44 @@ class ArbolBinario:
                 pygame.draw.line(pantalla, Negro, (nodo.x, nodo.y+NODO_TAMAÑO), (nodo.derecha.x, nodo.derecha.y-NODO_TAMAÑO), 3)     
                 self.DibujarNodo(pantalla, fuente, nodo.derecha)     
 
+class ArbolBinario2:     
+    def __init__(self):     
+        self.padre = None     
+
+        # Funcion que añade los valores a los nodos y creacion del arbol
+    def Añadirnodo(self, valor):     
+        if self.padre is None:     
+            self.padre = Nodo(valor, ANCHO/1.5, 50)     
+        else:     
+            self.AgregarNodo(valor, self.padre, ANCHO/1.5, 100, 1)     
+    def AgregarNodo(self, valor, nodo, x, y, nivel):     
+        if nodo.izquierda is None:     
+            nodo.izquierda = Nodo(valor, x-4*(6-nivel), y+NODO_TAMAÑO*2)     
+        elif nodo.derecha is None:     
+            nodo.derecha = Nodo(valor, x+4*(6-nivel), y+NODO_TAMAÑO*2)     
+        else:     
+            if valor < nodo.valor:     
+                self.AgregarNodo(valor, nodo.izquierda, x-2**(6-nivel), y+NODO_TAMAÑO*4, nivel+1)     
+            else:     
+                self.AgregarNodo(valor, nodo.derecha, x+2**(6-nivel), y+NODO_TAMAÑO*4, nivel+1)     
+    def draw(self, pantalla, fuente):     
+        self.DibujarNodo(pantalla, fuente, self.padre)     
+    def DibujarNodo(self, pantalla, fuente, nodo):     
+        if nodo is not None:     
+            pygame.draw.circle(pantalla, Amarillo, (nodo.x, nodo.y), NODO_TAMAÑO, 2)     
+            text_surface = fuente.render(str(nodo.valor), True, Amarillo)     
+            text_rect = text_surface.get_rect(center=(nodo.x, nodo.y))     
+            pantalla.blit(text_surface, text_rect)     
+            if nodo.izquierda is not None:     
+                pygame.draw.line(pantalla, Negro, (nodo.x, nodo.y+NODO_TAMAÑO), (nodo.izquierda.x, nodo.izquierda.y-NODO_TAMAÑO), 3)     
+                self.DibujarNodo(pantalla, fuente, nodo.izquierda)     
+            if nodo.derecha is not None:    
+                pygame.draw.line(pantalla, Negro, (nodo.x, nodo.y+NODO_TAMAÑO), (nodo.derecha.x, nodo.derecha.y-NODO_TAMAÑO), 3)     
+                self.DibujarNodo(pantalla, fuente, nodo.derecha) 
+                
+
 # Creamos el objeto arbol y le ajuntamos los nodos a el     
-arbol = ArbolBinario()     
+arbol = ArbolBinario1()     
 arbol.Añadirnodo(50)     
 arbol.Añadirnodo(30)     
 arbol.Añadirnodo(70)     
@@ -78,7 +117,22 @@ arbol.Añadirnodo(40)
 arbol.Añadirnodo(60)     
 arbol.Añadirnodo(80)     
 arbol.Añadirnodo(71)     
-#arbol.Añadirnodo(91)     
+arbol.Añadirnodo(29)
+#arbol.Añadirnodo(30) 
+#arbol.Añadirnodo(30)  
+#arbol.Añadirnodo(91)
+
+tree = ArbolBinario2()
+tree.Añadirnodo(5)
+tree.Añadirnodo(8)
+tree.Añadirnodo(19)
+tree.Añadirnodo(20)
+tree.Añadirnodo(23)
+tree.Añadirnodo(28)
+tree.Añadirnodo(10)
+tree.Añadirnodo(3)
+
+     
 
 # Hacemos el loop de la pantalla para que corra hasta que le demos a la X   
 Terminado = False     
@@ -91,7 +145,8 @@ while not Terminado:
     pantalla.fill(Purpura)     
 
    # Pintamos el arbol binario     
-    arbol.draw(pantalla, fuente)     
+    arbol.draw(pantalla, fuente)   
+    tree.draw(pantalla,fuente)  
 
    # Actualizamos la imagen (En este caso no es una funcion necesaria ya que no hay movimiento en la imagen)     
     pygame.display.flip()     
